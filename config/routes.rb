d16728c2b003f66b3b07e1f26e8ac34778d8cf39
akari yabana <subaru.barisaku@gmail.com>
2020-06-27 21:48:32 +0900

    header, footer整備, about作成, signup作成途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'home', to: 'embroideries#home'
-  get 'about', to: 'embroideries#about'
+  get '/home', to: 'embroideries#home'
+  get '/about', to: 'embroideries#about'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
   root 'embroideries#home'
+    resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
